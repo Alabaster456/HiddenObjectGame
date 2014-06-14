971a1459e3b1c460a81945f509330099b3b42833
Yazid <Alabaster456@users.noreply.github.com>
2014-06-14 09:33:32 -0700

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -2,42 +2,65 @@ $(document).ready(function(){
   
 });
 
-/* This is a list of sounds for the game. Each one has a purpose, and the variable names' shows it */
-
+//List of sounds for the game.
+var YES = new Audio("http://mobcup.co/dl/5EF93FF5F68B74F7245059A823B4018DB5DA7478E8381E06A9635A281E937756E755B90F08AAE5BCDB511F6BF9844E800F3C75F3ADD72D1ECDC75551587BB383D4045EF5E86A221AE121737F62727013E9581DDCDDA6A5B0F017EB693457C5904A3D146AE1A9A9F760C40BA4B27F5ADD9C68");
 var ghost = new Audio("http://soundjax.com/reddo/50125%5Eghost.mp3");
+
+//Audio that plays.
 ghost.loop = true;
 ghost.play();
+
 //END
 
 //Button actvations
 function Help(){
   alert("Welcome to Mysterious Findings!");
 alert("Find all of the hidden objects in each level with your mouse!");
-  alert("Items that you need to find will be displayed in the lower-right corner!");
+  alert("Each item that you need to find will be displayed in the lower-right corner!");
   alert("After you manage to find each item hidden, you will be taken to another level!");
   alert("Have fun!");
 }
-function Game(){
-  $(".disappear").fadeOut("slow");
-  ghost.pause();
-  $("#firstlevel").delay(1000).fadeIn("slow");
-  $(".lvl1").delay(1000).fadeIn("slow");
-  $("#clock").one("click",function(){
-    $("#clock").fadeOut("slow");
-    $("#clocklist").fadeOut("slow");
-  });
-  $("#ring").one("click",function(){
-  $("#ring").fadeOut("slow");
-    $("#ringlist").fadeOut("slow");
-    
-  });
-  $("#magnifyingglass").one("click",function(){
-    $("#magnifyingglass").fadeOut("slow");
-    $("#magnifyingglasslist").fadeOut("slow");
-  });
-  $("#hammer").one("click",function(){
-    $("#hammer").fadeOut("slow");
-    $("#hammerlist").fadeOut("slow");
-  });
-}
+
 //END
+function Game() {
+    $(".disappear").fadeOut("slow");
+  ghost.pause(); $("#firstlevel").delay(1000).fadeIn("slow");
+  $(".lvl1").delay(1000).fadeIn("slow");
+    var result = 0;
+    $("#clock, #ring, #hammer, #magnifyingglass").one("click", function () {
+        $(this).fadeOut('slow');
+      if (this.id === "clock") {
+$("#clocklist").fadeOut('slow');
+      } else if (this.id ==="ring") {
+        $("#ringlist").fadeOut('slow');
+      } else if (this.id === "hammer") {
+        $("#hammerlist").fadeOut('slow');
+      } else if (this.id === "magnifyingglass") {
+      $("#magnifyingglasslist").fadeOut('slow');  
+      }
+      
+        result++;
+        if (result === 4) {
+            $("#complete").delay(500).fadeIn('slow');
+          $(".nextlevel").delay(500).fadeIn('slow');
+          YES.play();
+        }
+    });
+}
+function Game2() {
+  $("#firstlevel").fadeOut('slow');
+ var result = 0;
+    $("#clock, #ring, #hammer, #magnifyingglass").one("click", function () {
+        $(this).fadeOut('slow');
+      if (this.id === "clock") {
+$("#clocklist").fadeOut('slow');
+      } else if (this.id ==="ring") {
+        $("#ringlist").fadeOut('slow');
+      } else if (this.id === "hammer") {
+        $("#hammerlist").fadeOut('slow');
+      } else if (this.id === "magnifyingglass") {
+      $("#magnifyingglasslist").fadeOut('slow');  
+}
+    });
+                                                      }
+
