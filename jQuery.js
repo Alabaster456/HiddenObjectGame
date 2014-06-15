9e04ba907099e425ff7e542069a789d227111c55
Yazid <Alabaster456@users.noreply.github.com>
2014-06-14 17:21:58 -0700

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
   
 });
-
-//List of sounds for the game.
 var YES = new Audio("http://mobcup.co/dl/5EF93FF5F68B74F7245059A823B4018DB5DA7478E8381E06A9635A281E937756E755B90F08AAE5BCDB511F6BF9844E800F3C75F3ADD72D1ECDC75551587BB383D4045EF5E86A221AE121737F62727013E9581DDCDDA6A5B0F017EB693457C5904A3D146AE1A9A9F760C40BA4B27F5ADD9C68");
-var ghost = new Audio("http://soundjax.com/reddo/50125%5Eghost.mp3");
+var firstlevel = new Audio("http://soundjax.com/reddo/27639%5EHouse_Wren.mp3");
 
-//Audio that plays.
+/* This is a list of sounds for the game. Each one has a purpose, and the variable names' shows it */
+//
+var ghost = new Audio("http://soundjax.com/reddo/50125%5Eghost.mp3");
 ghost.loop = true;
 ghost.play();
-
+//Remember to re-add the sound..
 //END
 
 //Button actvations
@@ -23,9 +23,12 @@ alert("Find all of the hidden objects in each level with your mouse!");
 
 //END
 function Game() {
+  firstlevel.loop = true;
+  firstlevel.play();
     $(".disappear").fadeOut("slow");
   ghost.pause(); $("#firstlevel").delay(1000).fadeIn("slow");
   $(".lvl1").delay(1000).fadeIn("slow");
+  $("#indicator").delay(1000).fadeIn('slow');
     var result = 0;
     $("#clock, #ring, #hammer, #magnifyingglass").one("click", function () {
         $(this).fadeOut('slow');
@@ -43,24 +46,38 @@ $("#clocklist").fadeOut('slow');
         if (result === 4) {
             $("#complete").delay(500).fadeIn('slow');
           $(".nextlevel").delay(500).fadeIn('slow');
+          firstlevel.pause();
           YES.play();
         }
     });
 }
 function Game2() {
   $("#firstlevel").fadeOut('slow');
+  $(".lvl1").fadeOut('slow');
+  $("#complete").fadeOut('slow');
+  $(".nextlevel").fadeOut('slow');
+  $(".lvl2").fadeIn('slow');
  var result = 0;
-    $("#clock, #ring, #hammer, #magnifyingglass").one("click", function () {
+    $("#footballhelmet, #microphone, #glue, #fishingpole, #guitar").one("click", function () {
         $(this).fadeOut('slow');
-      if (this.id === "clock") {
-$("#clocklist").fadeOut('slow');
-      } else if (this.id ==="ring") {
-        $("#ringlist").fadeOut('slow');
-      } else if (this.id === "hammer") {
-        $("#hammerlist").fadeOut('slow');
-      } else if (this.id === "magnifyingglass") {
-      $("#magnifyingglasslist").fadeOut('slow');  
+      if (this.id === "guitar") {
+$("#guitarlist").fadeOut('slow');
+      } else if (this.id ==="glue") {
+        $("#gluelist").fadeOut('slow');
+      } else if (this.id === "footballhelmet") {
+        $("#footballhelmetlist").fadeOut('slow');
+      } else if (this.id === "fishingpole") {
+      $("#fishingpolelist").fadeOut('slow');  
+} else if (this.id === "microphone") {
+  $("#microphonelist").fadeOut('slow');
 }
+            result++;
+        if (result === 5) {
+            $("#complete").delay(500).fadeIn('slow');
+          $(".nextlevel").delay(500).fadeIn('slow');
+        
+          YES.play();
+        }
     });
                                                       }
 
